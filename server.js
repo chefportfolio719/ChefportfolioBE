00e7fd8e0edcda41c2e5af50e90b60ae17042188
claridiva2000 <claridiva2000@gmail.com>
2019-07-17 20:42:02 -0500

    heroku problems

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -11,32 +11,30 @@ const server = express();
 const chefroutes = require('./routes/chefs');
 const dishroutes = require('./routes/recipes');
 
-
 server.use('/public/uploads', express.static('./public'));
 server.use(express.json());
-server.use(cors())
+server.use(cors());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(morgan('dev'));
 
-
 mongoose.connect(
- process.env.DB_CONNECT ,
+  '  mongodb+srv://chefportfolio2:backend@chefportfolio-8idgc.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true }
 );
-
 
 server.use('/chefs', chefroutes);
 server.use('/recipes', dishroutes);
 
-
-server.get('/', (req, res, next)=>{
-  res.send('<h1>ChefPortfolio</h1> <p>Welcome to our server.</p> <p>access chefs:</p> <p>https://chefportfoliopt4.herokuapp.com/chefs</p><p>access recipes:</p> <p>https://chefportfoliopt4.herokuapp.com/recipes</p> <p>to access individual recipes/chefs, just paste in the _id or click the link in the URL which is included for each recipe and chef</p> <p>access pictures: still working on that. we have pictures in memory, just not sure how to get them working just yet..</p>')
-})
+server.get('/', (req, res, next) => {
+  res.send(
+    '<h1>ChefPortfolio</h1> <p>Welcome to our server.</p> <p>access chefs:</p> <p>https://chefportfoliopt4.herokuapp.com/chefs</p><p>access recipes:</p> <p>https://chefportfoliopt4.herokuapp.com/recipes</p> <p>to access individual recipes/chefs, just paste in the _id or click the link in the URL which is included for each recipe and chef</p> <p>access pictures: still working on that. we have pictures in memory, just not sure how to get them working just yet..</p>'
+  );
+});
 
 server.use((req, res, next) => {
   const error = new Error('Not Found');
- res.status(404);
+  res.status(404);
   next(error);
 });
 
@@ -47,6 +45,5 @@ server.use((error, req, res, next) => {
     }
   });
 });
-
 
 module.exports = server;
